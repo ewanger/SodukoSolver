c7c5b466ea71aa98f9782d51d947e47e8a060eaa
Eric Wanger <eric@edw.com>
2014-04-06 22:49:40 -0500

    before trying to reiterate over tree.  still can't get def syntax right

--- SodukuSolver.rb
+++ SodukuSolver.rb
@@ -1,3 +1,5 @@
+
+
 #
 # Soduku Solver
 #
@@ -6,14 +8,14 @@
 #
 #  Initialize Data Structures
 #
-rows, cols = 9,9
-board = Array.new(rows) { Array.new(cols)  }
+rows, cols = 9, 9
+board = Array.new(rows) { Array.new(cols) }
 
 couldbe = Array.new(rows) { Array.new(cols) }
 
 couldbe.each_with_index do |row, y|
   row.each_index do |x|
-  couldbe[y][x] = [1,2,3,4,5,6,7,8,9]
+    couldbe[y][x] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 end
 
@@ -21,20 +23,30 @@ end
 #  Enter Puzzle
 #
 
-board[0] = [7,0,9,3,0,0,0,0,0]
-board[1] = [4,0,3,5,2,8,0,0,0]
-board[2] = [0,2,0,0,0,0,3,1,0]
-board[3] = [1,0,5,6,7,9,0,0,0]
-board[4] = [0,9,0,2,0,4,0,3,0]
-board[5] = [0,0,0,8,1,3,7,0,9]
-board[6] = [0,8,2,0,0,0,0,9,0]
-board[7] = [0,0,0,9,3,7,8,0,2]
-board[8] = [0,0,0,0,0,2,5,0,3]
+board[0] = [7, 0, 9, 3, 0, 0, 0, 0, 0]
+board[1] = [4, 0, 3, 5, 2, 8, 0, 0, 0]
+board[2] = [0, 2, 0, 0, 0, 0, 3, 1, 0]
+board[3] = [1, 0, 5, 6, 7, 9, 0, 0, 0]
+board[4] = [0, 9, 0, 2, 0, 4, 0, 3, 0]
+board[5] = [0, 0, 0, 8, 1, 3, 7, 0, 9]
+board[6] = [0, 8, 2, 0, 0, 0, 0, 9, 0]
+board[7] = [0, 0, 0, 9, 3, 7, 8, 0, 2]
+board[8] = [0, 0, 0, 0, 0, 2, 5, 0, 3]
 
 board.each_with_index do |row, y|
   print board[y].inspect
   puts
 end
+
+
+
+
+
+
+
+
+
+
 
 
 #
@@ -68,7 +80,7 @@ end
 board.each_with_index do |row, y|
   couldbe[y].each_with_index do |cell, x|
     if cell.count > 1
-     result = cell - row
+      result = cell - row
     else
       result = cell
     end
@@ -93,28 +105,27 @@ puts
 regionset = Array.new
 couldbeset = Array.new
 #then prune by region
-for region_y in 0..2
- for region_x in 0..2
-   for y in 0..2
-     for x in 0..2
-   regionset.push(board[region_y*3+y][region_x*3+x])
-     end
-   end
-   for y in 0..2
-     for x in 0..2
-       cell = couldbe[region_y*3+y][region_x*3+x]
-       if cell.count > 1
-         result = cell - regionset
-       else
-         result = cell
-       end
-       couldbe[region_y*3+y][region_x*3+x] = result
-     end
-   end
-   regionset = Array.new
-
-  end
-end
+(0..2).each { |region_y|
+  (0..2).each { |region_x|
+    (0..2).each { |y|
+      (0..2).each { |x|
+        regionset.push(board[region_y*3+y][region_x*3+x])
+      }
+    }
+    (0..2).each { |y|
+      (0..2).each { |x|
+        cell = couldbe[region_y*3+y][region_x*3+x]
+        if cell.count > 1
+          result = cell - regionset
+        else
+          result = cell
+        end
+        couldbe[region_y*3+y][region_x*3+x] = result
+      }
+    }
+    regionset = Array.new
+  }
+}
 
 #
 # Output Solution
@@ -125,7 +136,7 @@ puts
 couldbe.each_with_index do |row, y|
   printf "row: %d: ", y
   row.each_index do |x|
-    printf "%s " ,couldbe[y][x]
+    printf '%s ', couldbe[y][x]
   end
   puts
 end
